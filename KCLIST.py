701e7a1486508788d8edf443d1b08c113c65b7c6
PHO Vinh Son <phovinhson@pc-24.home>
2022-07-23 02:06:07 +0200

    fix issue with induced subgraph

--- KCLIST.py
+++ KCLIST.py
@@ -5,29 +5,36 @@ Article available here: https://oanabalalau.com/pdf/kcliques.pdf
 
 
 class vertex:
-    def __init__(self, identifier, degree, neighbor, label):
+    def __init__(self, identifier, neighbor, label):
         self.id = identifier
-        self.deg = degree
+        self.deg = 0
         self.neigh = neighbor
         self.label = label
+        self.degrees = [0 for _ in range(label)]
+
     
     def add_neigh(self, id_neigh):
         self.deg += 1
+        self.degrees[self.label-1] += 1
         self.neigh.append(id_neigh)
         
     def update_deg(self, degree):
         self.deg = degree
+        self.degrees[self.label-1] = degree
     
     def update_neigh(self,id_neigh):
         assert self.id < id_neigh
         self.neigh.insert(0, self.neigh.pop(self.neigh.index(id_neigh)))
+    
+    def revert_degree(self):
+        self.deg = self.degrees[self.label-1]
     
 class DAG_:
     def __init__(self,G,k):
         V,E = G
         self.adj = []
         for i in range(len(V)):
-            self.adj.append(vertex(i,0,[],k))
+            self.adj.append(vertex(i,[],k))
         
         for u,v in E:
             if u>v : u,v = v,u
@@ -52,37 +59,32 @@ def induced_DAG(DAG,u,l):
                 DAG.adj[v].update_neigh(w)
                 new_deg += 1
         DAG.adj[v].update_deg(new_deg)
-        
 
-def listing(l,DAG,C,res, verbose = False):
-    if verbose:        
-        print()
-        print()
-        print("call listing")
-        print("l =",l)
-        print("|C| =",len(C))
-        print()
+def listing(l,DAG,C,res):
     if l == 2:
         for u in DAG.adj: #listing all nodes
             if u.label == l: #if u is in the correct subgraph
                 for i in range(u.deg):
-                    if verbose: print(C|{u.id,u.neigh[i]})
+                    
                     res.append(C|{u.id,u.neigh[i]})
     else:
         for u in DAG.adj:
             if u.label == l:
+
                 induced_DAG(DAG,u.id,l) #labeling
-                listing(l-1,DAG,C|{u.id},res,verbose)
-                
+               
+                listing(l-1,DAG,C|{u.id},res)
+
                 for i in range(u.deg): #relabeling
                     v = u.neigh[i]
                     DAG.adj[v].label = l
+                    DAG.adj[v].revert_degree()
 
-def KCLIST(G,k,verbose = False):
+def KCLIST(G,k):
     """
     Return the list of all cliques of size k of G = (V,E) and V = {i for i in range(|V|)}
     """
     DAG = DAG_(G,k)
     res = []
-    listing(k,DAG,set(),res,verbose)
+    listing(k,DAG,set(),res)
     return res
